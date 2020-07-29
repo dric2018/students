882592a542501c07bac742600dfc51cb3271f015
dric2018 <manouancedric@gmail.com>
2020-07-29 02:26:08 +0000

    adding Wrappers to PAgeEtudiants frame

--- interface.py
+++ interface.py
@@ -1,6 +1,28 @@
 import tkinter as tk 
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 
+import mysql.connector as mc 
+
+BD_NAME = "students"
+
+"""
+# parametres de connexion à la base de données
+ma_bd = mc.connect(
+    host = "localhost",
+    user = "root",
+    passwd ="password",
+    database = BD_NAME
+)
+
+#curseur d'execution des requetes SQL
+mon_curseur = ma_bd.cursor()
+
+# mysql requetes 
+"""
+
+# mysql requetes
 
 LARGE_FONT = ("Verdana", 12)
 
@@ -8,7 +30,8 @@ LARGE_FONT = ("Verdana", 12)
 class AppStudent(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        self.resizable = (False, False)
+        self.resizable(False, False)
+        self.geometry("1200x650")
 
         container = tk.Frame(self)
 
@@ -47,11 +70,11 @@ class PageAccueil(tk.Frame):
         label.pack(padx=10, pady=10)
 
 
-        btn_1 = tk.Button(self, text=" Etudinats ", command=lambda: controller.show_frame(PageEtudiants))
-        btn_2 = tk.Button(self, text=" Notes ", command=lambda: controller.show_frame(PageNotes))
+        btn_1 = tk.Button(self, text=" Etudinats ",padx=20, pady=10, command=lambda: controller.show_frame(PageEtudiants))
+        btn_2 = tk.Button(self, text=" Notes ", padx=20, pady=10, command=lambda: controller.show_frame(PageNotes))
 
-        btn_1.pack()
-        btn_2.pack()
+        btn_1.pack( side=tk.BOTTOM)
+        btn_2.pack(side=tk.BOTTOM)
 
 # page Notes
 class PageNotes(tk.Frame):
@@ -60,20 +83,75 @@ class PageNotes(tk.Frame):
         label = tk.Label(self, text = "Page Notes", font=LARGE_FONT)
         label.pack(padx=10, pady=10)
 
+        wrapper1 = LabelFrame(self, text="Liste étudiants & Notes")
+        wrapper2 = LabelFrame(self, text="Rechercher")
+        wrapper3 = LabelFrame(self, text="Données Etudiant & Notes")
+
+        wrapper1.pack(fill="both", expand="yes", padx=20, pady=10)
+        wrapper2.pack(fill="both", expand="yes", padx=20, pady=10)
+        wrapper3.pack(fill="both", expand="yes", padx=20, pady=10)
 
         btn_1 = tk.Button(self, text=" Accueil ", command=lambda: controller.show_frame(PageAccueil))
         btn_2 = tk.Button(self, text=" Etudiants ", command=lambda: controller.show_frame(PageEtudiants))
 
-        btn_1.pack()
-        btn_2.pack()
+        btn_1.pack(side=tk.BOTTOM)
+        btn_2.pack(side=tk.BOTTOM)
 
 # page Etudiants
+def renvoyer(lignes):
+    tree.delete(*tree.get_children())
+    for ligne in lignes:
+        tree.insert('', 'end', ligne)
+
+def RecherCherEtudiant():
+    r = q.get()
+    req = "SELECT Matricule, Nom, Prenoms, Genre, Date_naissance, Lieu_naissance, Email\
+         FROM Etudiants \
+         WHERE Prenoms LIKE '%"+q+"'% OR Nom LIKE '%"+q+"'%"
+    mon_curseur.execute(req)
+    result = mon_curseur.fetchall()
+    renvoyer(result)
+
+
 class PageEtudiants(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text = "Page Etudiants", font=LARGE_FONT)
         label.pack(padx=10, pady=10)
 
+        # les wrapers
+        wrapper1 = LabelFrame(self, text="Liste étudiants")
+        wrapper2 = LabelFrame(self, text="Rechercher")
+        wrapper3 = LabelFrame(self, text="Données Etudiant")
+
+        wrapper1.pack(fill="both", expand="yes", padx=20, pady=10)
+        wrapper2.pack(fill="both", expand="yes", padx=20, pady=10)
+        wrapper3.pack(fill="both", expand="yes", padx=20, pady=10)
+
+        # les treeView 
+        tree = ttk.Treeview(wrapper1, columns=(1, 2, 3, 4, 5, 6, 7), show="headings", height=9)
+        tree.pack()
+        tree.heading(1, text="Matricule")
+        tree.heading(2, text="Nom")
+        tree.heading(3, text="Prenom(s)")
+        tree.heading(4, text="Genre")
+        tree.heading(5, text="Date de naissance")
+        tree.heading(6, text="Lieu de naissance")
+        tree.heading(7, text="Email")
+
+        # barre de recherche
+        label_recherche_etudiant = Label(wrapper2, text="Rechercher")
+        label_recherche_etudiant.pack(side=tk.LEFT, padx = 10)
+
+        q = StringVar()
+
+        champs_saisie1 = Entry(wrapper2, textvariable= q)
+        champs_saisie1.pack(side=tk.LEFT, padx=10)
+        btn_rechercher_etudiant = Button(wrapper2, text="Rechercher", command=RecherCherEtudiant)
+        btn_rechercher_etudiant.pack(side=tk.LEFT, padx=6)
+        # donnees etudiant
+
+       
 
         btn_1 = tk.Button(self, text=" Accueil ", command=lambda: controller.show_frame(PageAccueil))
         btn_2 = tk.Button(self, text=" Notes ", command=lambda: controller.show_frame(PageNotes))
