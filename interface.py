4e27816b520c531ae5f754635c3db2159de07e81
dric2018 <manouancedric@gmail.com>
2020-07-30 10:48:51 +0000

    adding PageNotes features

--- interface.py
+++ interface.py
@@ -88,6 +88,7 @@ class PageNotes(tk.Frame):
         label = tk.Label(self, bg="gold", text = "Page Notes", font=LARGE_FONT)
         label.pack(padx=10, pady=10)
 
+        # wrappers
         wrapper1 = LabelFrame(self, text="Tableau des notes")
         wrapper2 = LabelFrame(self, text="Rechercher")
         wrapper3 = LabelFrame(self, text="Bulletin Etudiant & Notes")
@@ -95,16 +96,345 @@ class PageNotes(tk.Frame):
         wrapper1.pack(fill="both", expand="yes", padx=20, pady=10)
         wrapper2.pack(fill="both", expand="yes", padx=20, pady=10)
         wrapper3.pack(fill="both", expand="yes", padx=20, pady=10)
+
+        self.q = tk.StringVar() # champ rechercher
+        self.c1 = tk.StringVar()
+        self.c2 = tk.StringVar()
+        self.c3 = tk.StringVar()
+        self.c4 = tk.StringVar()
+        self.c5 = tk.StringVar()
+        self.c6 = tk.StringVar()
+        self.c7 = tk.StringVar()
+        self.c8 = tk.StringVar()
+        self.c9 = tk.StringVar()
+        self.c10 = tk.StringVar()
+        self.c11 = tk.StringVar()
+        self.c12 = tk.StringVar()
+
+        # fonctions
+        def update(lignes):
+            tree.delete(*tree.get_children())
+            for ligne in lignes:
+                tree.insert('', 'end', values=ligne)
+
+
+
+
+        def RecherNote():
+            req = f"SELECT SELECT Note_id, Matricule_Etud, etudiants.Nom, etudiants.Prenoms, \
+            etudiants.Sexe, etudiants.Date_naissance, etudiants.Lieu_naissance, Note_1, Note_2, Note_3, Note_4, Note_5 \
+            FROM Notes \
+            WHERE etudiants.Prenoms LIKE '%{self.q.get().lower()}%' OR etudiants.Nom LIKE '%{self.q.get().upper()}%' \
+            INNER JOIN `etudiants` ON notes.Matricule_Etud = etudiants.Matricule \
+            ORDER BY Matricule_Etud ASC"            
+            
+            mon_curseur.execute(req)
+            result = mon_curseur.fetchall()
+            update(result)
+
+
+        def effacer():
+
+            req_all_notes = "SELECT Note_id, Matricule_Etud, etudiants.Nom, etudiants.Prenoms, \
+            etudiants.Sexe, etudiants.Date_naissance, etudiants.Lieu_naissance, Note_1, Note_2, Note_3, Note_4, Note_5 \
+            FROM Notes \
+            INNER JOIN `etudiants` ON notes.Matricule_Etud = etudiants.Matricule \
+            ORDER BY Matricule_Etud ASC"
+            
+            mon_curseur.execute(req_all_notes)
+            lignes = mon_curseur.fetchall()
+            update(lignes)
+            self.q.set("")
+            self.c1.set("")
+            self.c2.set("")
+            self.c3.set("")
+            self.c4.set("")
+            self.c5.set("")
+            self.c6.set("")
+            self.c7.set("")
+            self.c9.set("")
+            self.c10.set("")
+            self.c11.set("")
+            self.c12.set("")
+            self.c8.set("")
+
+
+        def tous_les_champs_ok():
+
+            note1 = self.c2.get()
+            note_2 = self.c3.get()
+            note_3 = self.c4.get()
+            note_4 = self.c5.get()
+            note_5 = self.c6.get()
+            matricule = self.c1.get()
+            
+            ch = [note1, note2, note3, note4, note5]
+            for champ in ch:
+                if champ == "" or champ == " ":
+                    return False
+                else :
+                    return True
+
+        def ajouter_etudiant():
+            
+            if tous_les_champs_ok() and messagebox.askyesno(title="Ajout note", message=" Voulez-vous ajouter cette notation ?"):
+                
+                req_insertion_note = f"INSERT INTO notes (Matricule_Etud, Note_1, Note_2, Note_3, Note_4, Note_5)\
+                                VALUES (%s, %s, %s, %s, %s, %s)"
+
+                try:
+                    matricule = self.c7.get()
+                    note1 = self.c2.get()
+                    note_2 = self.c3.get()
+                    note_3 = self.c4.get()
+                    note_4 = self.c5.get()
+                    note_5 = self.c6.get()
+
+                    new_note = (matricule, note1, note2, note3, note4, note5)
+                    try:
+                        mon_curseur.execute(req_insertion_note, new_note)
+                        ma_bd.commit()
+                        print("[INFO] Nouvelle note enregistrée !")
+
+                    except Exception as e:
+                        print(e)
+                    print("[INFO] Nouvelle notation enregistrée !")
+                    messagebox.showinfo(title="Enregistrement note", message=f"La notation de l'etudiant {matricule} a été enregistrée !")
+                    
+                    ma_bd.commit()
+                    effacer()
+
+                except Exception as e:
+                    print("[INFO]", e)
+                    messagebox.showerror(title="Enregistrement note", message=f"Erreur lors de l'enregistrement !")
+
+            else:
+                messagebox.showerror(title="Enregistrement note", message=f"Erreur lors de l'enregistrement !")
+                messagebox.showwarning(title="Enregistrement note", message=f"Assurez-vous que tous les champs sont bien renseignés !")
+                return True
+
+
+        def supprimer_note():
+            matricule = self.c1.get()
+
+            if tous_les_champs_ok() and messagebox.askyesno(title="Suppression de note", message=" Voulez-vous supprimer cet étudiant ?"):
+                req = "DELETE FROM Etudiants WHERE Matricule = %s"
+                mon_curseur.execute(req, (matricule,))
+                ma_bd.commit()
+
+                messagebox.showinfo(title="Suppression de note", message=f"Notes supprimées !")
+                effacer()
+            else:
+                messagebox.showerror(title="Suppression de note", message=f"l'opération a échoué !")
+                messagebox.showwarning(title="Suppression de note", message=f"Assurez-vous que tous les champs sont bien renseignés !")
+                return True
+
+
+        def modifier_note():
+
+            if tous_les_champs_ok() and messagebox.askyesno(title="Modification de note", message=" Voulez-vous modifier les notes de cet étudiant ?"):
+
+                note_id = self.c1.get()
+                note_1 = self.c2.get()
+                note_2 = self.c3.get()
+                note_3 = self.c4.get()
+                note_4 = self.c5.get()
+                note_5 = self.c6.get()
+                matricule = self.c7.get()
+
+
+                new_note = (matricule, note1, note2, note3, note4, note5)
+
+
+                req = "UPDATE Etudiants SET Nom=%s, Prenoms=%s, Sexe=%s, Date_naissance=%s, Lieu_naissance=%s, Nationalite=%s, Telephone=%s, Email=%s WHERE Matricule = %s"
+                mon_curseur.execute(req, new_note)
+                ma_bd.commit()
+
+                # afficher un pop-up de succes
+                messagebox.showinfo(title="Modification de notes ", message="Modification enregistrée !")
+                effacer()
+                
+                print("[INFO] Modification étudiant enregistrée !")
+
+            else:
+                messagebox.showerror(title="Modification de notes", message="La modification a échoué !")
+                messagebox.showwarning(title="Modification de notes", message=f"Assurez-vous que tous les champs sont renseignés !")
+                return True
+
+
+        ################# afficher les  notes 
+
+        req_bulletin = "SELECT etudiants.Matricule, etudiants.Nom, etudiants.Prenoms, etudiants.Sexe, etudiants.Date_naissance, \
+            etudiants.Lieu_naissance, etudiants.Nationalite, etudiants.Telephone, etudiants.Email, notes.Note_1, notes.Note_2, \
+            notes.Note_3, notes.Note_4, notes.Note_5 \
+            FROM `notes` \
+            INNER JOIN `etudiants` ON notes.Matricule_Etud = etudiants.Matricule "
+
         
+        # fonctions 
+        def get_ligne(event):
+            id_ligne = tree.identify_row(event.y)
+            selection = tree.item(tree.focus())
+            self.c1.set(selection['values'][0])
+            self.c2.set(selection['values'][1])
+            self.c3.set(selection['values'][2])
+            self.c4.set(selection['values'][3])
+            self.c5.set(selection['values'][4])
+            self.c6.set(selection['values'][5])
+            self.c7.set(selection['values'][6])
+            self.c8.set(selection['values'][2])
+            self.c9.set(selection['values'][8])
+            self.c10.set(selection['values'][9])
+            self.c11.set(selection['values'][10])
+            self.c12.set(selection['values'][11])
+
+            print(f"[INFO] Ligne {id_ligne} sélectionnée")
+
+        # le treeView 
+        tree = ttk.Treeview(wrapper1, columns=(1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12), 
+                            show="headings", height=5)
+
+        tree.pack(side=tk.LEFT)
+        tree.place(x=0, y=0)
+
+        style = ttk.Style(tree)
+        style.configure("Treeview", rowheight=15)
+        tree.heading("#0", text="")
+        tree.heading("#1", text="NoteID")
+        tree.heading("#2", text="Matricule")
+        tree.heading("#3", text="Nom")
+        tree.heading("#4", text="Prenom(s)")
+        tree.heading("#5", text="Sexe")
+        tree.heading("#6", text="Né le")
+        tree.heading("#7", text="A")        
+        tree.heading("#8", text="Note 1")
+        tree.heading("#9", text="Note 2")
+        tree.heading("#10", text="Note 3")
+        tree.heading("#11", text="Note 4")
+        tree.heading("#12", text="Note 5")
+
+        # taille des colonnes 
+        tree.column("#1", width=50, minwidth=50, stretch=False)
+        tree.column("#2", width=90, minwidth=90, stretch=False)
+        tree.column("#3", width=90, minwidth=90, stretch=False)
+        tree.column("#4", width=120, minwidth=120, stretch=False)
+        tree.column("#5", width=50, minwidth=50, stretch=False)
+        tree.column("#6", width=70, minwidth=70, stretch=False)
+        tree.column("#7", width=113, minwidth=113, stretch=False)
+        tree.column("#8", width=50, minwidth=50, stretch=False)
+        tree.column("#9", width=50, minwidth=50, stretch=False)
+        tree.column("#10", width=50, minwidth=50, stretch=False)
+        tree.column("#11", width=50, minwidth=50, stretch=False)
+        tree.column("#12", width=50, minwidth=50, stretch=False)
+
+        #scrollBars
+        yscrollbar = ttk.Scrollbar(wrapper1, orient="vertical", command=tree.yview)
+        yscrollbar.pack(side=tk.RIGHT, fill="y")
+
+        xscrollbar = ttk.Scrollbar(wrapper1, orient="horizontal", command=tree.xview)
+        xscrollbar.pack(side=tk.BOTTOM, fill="x")
+
+        tree.configure(yscrollcommand=yscrollbar.set, xscrollcommand=xscrollbar.set)
+
+        req_all_notes = "SELECT Note_id, Matricule_Etud, etudiants.Nom, etudiants.Prenoms, \
+            etudiants.Sexe, etudiants.Date_naissance, etudiants.Lieu_naissance, Note_1, Note_2, Note_3, Note_4, Note_5 \
+            FROM Notes \
+            INNER JOIN `etudiants` ON notes.Matricule_Etud = etudiants.Matricule \
+            ORDER BY Matricule_Etud ASC"
+
+        mon_curseur.execute(req_all_notes)
+        lignes = mon_curseur.fetchall()
+        nb_lignes = mon_curseur.rowcount
+        # affichage de la liste
+        for ligne in lignes:
+             tree.insert('', 'end', values=ligne)
+
+        # en cas de double-click sur un éleve
+        tree.bind("<Double-1>", get_ligne)
+
+        ######### barre de recherche
+        label_recherche_etudiant = Label(wrapper2, text="Rechercher")
+        label_recherche_etudiant.pack(side=tk.LEFT, padx = 10)
+
+
+        champs_saisie1 = Entry(wrapper2, textvariable= self.q)
+        champs_saisie1.pack(side=tk.LEFT, padx=10, ipadx=30)
+        btn_rechercher_etudiant = Button(wrapper2, bg="white", text="Rechercher", command=RecherNote)
+        
+        btn_effacer_recherche = Button(wrapper2, bg="white", text="Effacer", command=effacer)
+        btn_effacer_recherche.pack(side=tk.LEFT, padx=20, pady=10)
+        btn_rechercher_etudiant.pack(side=tk.LEFT, padx=10, pady=5)  
+
+        #################
+        ######## bulletin 
+        #      
+        # champs de recuperation de données 
+        label1 = Label(wrapper3, text="Matricule")
+        label1.grid(row=0, column =0, padx=5, pady=3)
+        champs1 = Entry(wrapper3, textvariable=self.c1)
+        champs1.grid(row=0, column =1, padx=10, pady=3, ipadx=30)
+
+
+        label2 = Label(wrapper3, text="Nom")
+        label2.grid(row=1, column =0, padx=10, pady=3)
+        champs2 = Entry(wrapper3, textvariable=self.c2)
+        champs2.grid(row=1, column =1, padx=5, pady=3, ipadx=30)
+
+        label3 = Label(wrapper3, text="Prenoms")
+        label3.grid(row=2, column =0, padx=20, pady=3)
+        champs3 = Entry(wrapper3, textvariable=self.c3)
+        champs3.grid(row=2, column =1, padx=5, pady=3, ipadx=30)
+
+
+        label4 = Label(wrapper3, text="Sexe")
+        label4.grid(row=3, column =0, padx=5, pady=3)
+        champs4 = Entry(wrapper3, textvariable=self.c4)
+        champs4.grid(row=3, column =1, padx=5, pady=3, ipadx=30)
+
+        label5 = Label(wrapper3, text="Date de naissance")
+        label5.grid(row=4, column =0, padx=5, pady=3)
+        champs5 = Entry(wrapper3, textvariable=self.c5)
+        champs5.grid(row=4, column =1, padx=5, pady=3, ipadx=30)
+
+        label6 = Label(wrapper3, text="Lieu de naissance")
+        label6.grid(row=5, column =0, padx=10, pady=3)
+        champs6 = Entry(wrapper3, textvariable=self.c6)
+        champs6.grid(row=5, column =1, padx=5, pady=3, ipadx=30)
+
+        label7 = Label(wrapper3, text="Note 1")
+        label7.grid(row=6, column =0, padx=5, pady=3)
+        champs7 = Entry(wrapper3, textvariable=self.c7)
+        champs7.grid(row=6, column =1, padx=5, pady=3, ipadx=30)
+
+        label8 = Label(wrapper3, text="Note 2")
+        label8.grid(row=7, column =0, padx=10, pady=3)
+        champs8 = Entry(wrapper3, textvariable=self.c8)
+        champs8.grid(row=7, column =1, padx=5, pady=3, ipadx=30)
+
+        label9 = Label(wrapper3, text="Note 3")
+        label9.grid(row=8, column =0, padx=15, pady=3)
+        champs9 = Entry(wrapper3, textvariable=self.c9)
+        champs9.grid(row=8, column =1, padx=5, pady=3, ipadx=30)        
+
+        label8 = Label(wrapper3, text="Note 4")
+        label8.grid(row=9, column =0, padx=10, pady=3)
+        champs8 = Entry(wrapper3, textvariable=self.c10)
+        champs8.grid(row=9, column =1, padx=5, pady=3, ipadx=30)
+
+        label9 = Label(wrapper3, text="Note 5")
+        label9.grid(row=10, column =0, padx=15, pady=3)
+        champs9 = Entry(wrapper3, textvariable=self.c11)
+        champs9.grid(row=10, column =1, padx=5, pady=3, ipadx=30)  
+
         # Boutons de navigation
-        btn_1 = tk.Button(self, bg="white", text=" Notes ",padx=25, pady=6, command=lambda: controller.show_frame(PageNotes))
-        btn_2 = tk.Button(self, bg="white",  text=" Etudiants ", padx=20, pady=6, command=lambda: controller.show_frame(PageEtudiants))
+        btn_1 = tk.Button(self, bg="white", text=" Etudiants ",padx=25, pady=6, command=lambda: controller.show_frame(PageEtudiants))
+        btn_2 = tk.Button(self, bg="white",  text=" Accueil ", padx=20, pady=6, command=lambda: controller.show_frame(PageAccueil))
 
         btn_1.pack( side=tk.BOTTOM)
         btn_2.pack(side=tk.BOTTOM)
 
-        btn_1.place(x=325, y=500)
-        btn_2.place(x=425, y=500)
+        btn_1.place(x=325, y=650)
+        btn_2.place(x=450, y=650)
 
 
 
--- partie_2.py
+++ partie_2.py
@@ -1,1 +1,287 @@
-i
+# importations 
+import tkinter as tk
+import mysql.connector as mc
+
+BD_NAME = "students"
+
+
+# parametres de connexion à la base de données
+ma_bd = mc.connect(
+    host = "localhost",
+    user = "root",
+    passwd ="",
+    database = BD_NAME
+)
+
+#curseur d'execution des requetes SQL
+mon_curseur = ma_bd.cursor()
+
+# requetes
+req_creation_bd = f"CREATE DATABASE IF NOT EXISTS {BD_NAME}"
+req_creation_table_etudiants = "CREATE TABLE IF NOT EXISTS Etudiants \
+                                (Matricule varchar(255) NOT NULL, \
+                                Nom varchar(255) NOT NULL, \
+                                Prenoms varchar(255) NOT NULL, \
+                                Sexe varchar(2) NOT NULL, \
+                                Date_naissance DATE NOT NULL, \
+                                Lieu_naissance varchar(255) NOT NULL, \
+                                Nationalite varchar(255) NOT NULL, \
+                                Telephone varchar(45), \
+                                Email varchar(255) NOT NULL, \
+                                PRIMARY KEY (Matricule)) \
+                            "
+req_creation_table_notes = "CREATE TABLE IF NOT EXISTS Notes \
+                                (Note_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,\
+                                Note_1 int NULL,\
+                                Note_2 int NULL, \
+                                Note_3 int NULL, \
+                                Note_4 int NULL, \
+                                Note_5 int NULL, \
+                                Matricule_Etud varchar(255) NOT NULL,  \
+                                CONSTRAINT FK_EtudiantNote FOREIGN KEY (Matricule_Etud)\
+                                REFERENCES etudiants(Matricule)\
+                                ON DELETE CASCADE\
+                                ON UPDATE CASCADE)"
+# etudiants a ajouter a la creation
+etudiants_a_ajouter = [
+    ('20-EFCPC-1', 'YAO', 'marc', 'M', "1984-5-21", 'Abengourou', 'ivoirienne', '07070707', 'marc.yao@efcpc.ci'),
+    ('20-EFCPC-2', 'OUATTARA', 'kanigui moussa', 'M', "1994-8-17", 'Abobo', 'ivoirienne', '07080922', 'moussa.ouattara@efcpc.ci'),
+    ('20-EFCPC-3', 'LEGER', 'roxana', 'F', "1991-2-10", 'Katiola', 'ivoirienne', '01070207', 'roxana.leger@efcpc.ci'),
+    ('20-EFCPC-4', 'KOUAKOU', 'elsa', 'F', "1991-3-12", 'Man', 'ivoirienne', '02151448', 'elsa.kouakou@efcpc.ci'),
+    ('20-EFCPC-5', 'DJELA', 'abby faustin', 'M', "1986-10-15", 'Kong', 'ivoirienne', '05124514', 'faustin.djela@efcpc.ci'),
+    
+]
+
+# notes a ajouter a la creation 
+notes_a_ajouter = [
+    ('20-EFCPC-5', 14, 18, 12.5, 10, 19),
+    ('20-EFCPC-2', 16, 4, 10.5, 9, 11),
+    ('20-EFCPC-1', 7, 5, 8.5, 18, 10),
+    ('20-EFCPC-7', 12, 10, 5.5, 9, 14.5),
+    ('20-EFCPC-3', 14, 14, 12.5, 13, 14),
+    
+]
+
+req_show_db = "SHOW DATABASES"
+req_show_tables = "SHOW TABLES"
+
+
+def inserer_etudiant(nom, 
+                    prenoms, 
+                    sexe, 
+                    date_naissance, 
+                    lieu_naissance, 
+                    nationalite, 
+                    telephone, 
+                    email):
+
+    req_insertion_etudiant = f"INSERT INTO Etudiants (Matricule, Nom, Prenoms, Sexe, \
+                                Date_naissance, Lieu_naissance, Nationalite, Telephone, Email)\
+                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+    # recuperation du nombre de lignes enregistrées
+    mon_curseur.execute("SELECT COUNT(*) FROM Etudiants")
+    num = mon_curseur.fetchone()[0]
+
+    matricule = f"20-EFCPC-{num + 1}"
+
+    etudiant = (matricule, 
+                nom.upper(), 
+                prenoms.lower(), 
+                sexe, 
+                date_naissance, 
+                lieu_naissance, 
+                nationalite.lower(), 
+                telephone, 
+                email)
+    try:
+        mon_curseur.execute(req_insertion_etudiant, etudiant)
+        ma_bd.commit()
+        print("[INFO] Nouvel étudiant enregistré !")
+    except Exception as e:
+        print(e)
+
+
+def inserer_note(matricule_etud, note1, note2, note3, note4, note5):
+
+    req_insertion_note = f"INSERT INTO notes (Matricule_Etud, Note_1, Note_2, Note_3, Note_4, Note_5)\
+                                VALUES (%s, %s, %s, %s, %s, %s)"
+
+    # recuperation du nombre de lignes enregistrées
+
+    new_note = (matricule_etud, note1, note2, note3, note4, note5)
+    try:
+        mon_curseur.execute(req_insertion_note, new_note)
+        ma_bd.commit()
+        print("[INFO] Nouvelle note enregistrée !")
+
+    except Exception as e:
+        print(e)
+
+
+
+
+def lister_etudiants():
+    req_lister_etudiants = "SELECT * FROM Etudiants ORDER BY Nom ASC"
+
+    mon_curseur.execute(req_lister_etudiants)
+    result = mon_curseur.fetchall()
+
+    return result
+
+
+def lister_notes():
+    req_lister_notes = "SELECT * FROM Notes"
+
+    mon_curseur.execute(req_lister_notes)
+    result = mon_curseur.fetchall()
+
+    return result
+
+
+def check_table(table="Etudiants"):
+    mon_curseur.execute(f"USE {BD_NAME}")
+    mon_curseur.execute(req_show_tables)
+
+    tables = [table[0] for table in mon_curseur]
+
+    if table in tables :
+        print(f'Table {table} Trouvée !')
+        return True
+    else :
+        print(f"Table {table} non trouvée")
+        return False
+
+
+
+
+def check_bd(nom_bd="students"):
+
+    mon_curseur.execute(req_show_db)
+    bases_de_donnees = [db[0] for db in mon_curseur]
+    if nom_bd in bases_de_donnees :
+        print(f'Base de données {nom_bd} Trouvée !')
+        return True
+    else :
+        print("Base de données etudiants non trouvée")
+        return False
+
+
+
+def init_projet(etudiants_a_ajouter):
+    existe = check_bd()
+
+    if existe:
+        table_existe = check_table(table="etudiants")
+        if table_existe:
+            liste_etudiants = lister_etudiants()
+           
+        else :
+            print("[INFO] Creation table Etudiants...")
+            try:
+                mon_curseur.execute(req_creation_table_etudiants)
+                # ajouter les premiers etudiants
+                for etudiant in etudiants_a_ajouter:
+                    mat = etudiant[0]
+                    nom = etudiant[1]
+                    prenoms = etudiant[2]
+                    sexe = etudiant[3]
+                    date_naiss = etudiant[4]
+                    lieu_naiss = etudiant[5]
+                    nationalite = etudiant[6]
+                    telephone = etudiant[7]
+                    email = etudiant[8]
+
+                    inserer_etudiant(nom=nom, 
+                                    prenoms=prenoms, 
+                                    sexe=sexe, 
+                                    date_naissance=date_naiss, 
+                                    lieu_naissance=lieu_naiss, 
+                                    nationalite=nationalite, 
+                                    telephone=telephone, 
+                                    email=email)
+                                    
+
+                print("[INFO] Table Etudiants créée avec succès...")
+
+            except Exception as e:
+                print(e)
+
+        table_note_existe = check_table(table="notes")
+        if  table_note_existe:
+            liste_notes = lister_notes()
+           
+        else :
+            print("[INFO] Creation table Notes...")
+            try:
+                mon_curseur.execute(req_creation_table_notes)
+                # ajouter les premieres notes
+                for note in notes_a_ajouter:
+                    matri = note[0]
+                    note1 = note[1]
+                    note2 = note[2]
+                    note3 = note[3]
+                    note4 = note[4]
+                    note5 = note[5]
+
+
+                    inserer_note(matricule_etud=matri, 
+                                        note1=note1, 
+                                        note2=note2,
+                                        note3=note3,
+                                        note4=note4, 
+                                        note5=note5)
+
+                print("[INFO] Table Notes créée avec succès...")
+
+            except Exception as e:
+                print(e)
+    else :
+        mon_curseur.execute(req_creation_bd)
+        mon_curseur.execute(f"USE {BD_NAME}")
+        mon_curseur.execute(req_creation_table_etudiants)
+        mon_curseur.execute(req_creation_table_notes)
+
+
+        # ajouter les premiers etudiants
+        for etudiant in etudiants_a_ajouter:
+            mat = etudiant[0]
+            nom = etudiant[1]
+            prenoms = etudiant[2]
+            sexe = etudiant[3]
+            date_naiss = etudiant[4]
+            lieu_naiss = etudiant[5]
+            nationalite = etudiant[6]
+            telephone = etudiant[7]
+            email = etudiant[8]
+
+            inserer_etudiant(nom=nom, 
+                            prenoms=prenoms, 
+                            sexe=sexe, 
+                            date_naissance=date_naiss, 
+                            lieu_naissance=lieu_naiss, 
+                            nationalite=nationalite, 
+                            telephone=telephone, 
+                            email=email)
+
+         # ajouter les premieres notes
+        for note in notes_a_ajouter:
+            matri = note[0]
+            note1 = note[1]
+            note2 = note[2]
+            note3 = note[3]
+            note4 = note[4]
+            note5 = note[5]
+
+
+            inserer_note(matricule_etud=matri, 
+                        note1=note1, 
+                        note2=note2,
+                        note3=note3,
+                        note4=note4, 
+                        note5=note5)
+# initialisation du projet
+init_projet(etudiants_a_ajouter=etudiants_a_ajouter)
+
+                            
+
